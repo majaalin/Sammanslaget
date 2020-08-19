860451685d2e8daa27f27632b90ae7d5ec463f8a
majaalin <maja_alin@hotmail.com>
2020-08-19 18:51:40 +0200

    test

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,23 +1,23 @@
 import '../styles/index.scss';
 import SmoothScroll from 'smooth-scroll';
 
-// const pil = document.querySelector('.button-wrapper');
-const flip = document.querySelector('.flip-card-inner');
-const affisch = document.querySelectorAll('.affisch');
+// // const pil = document.querySelector('.button-wrapper');
+// const flip = document.querySelector('.flip-card-inner');
+// const affisch = document.querySelectorAll('.affisch');
 
-// pil.addEventListener('click', (event) => {
-//   flip.classList.toggle('flip-card-inner-hover');
-// });
+// // pil.addEventListener('click', (event) => {
+// //   flip.classList.toggle('flip-card-inner-hover');
+// // });
 
-affisch.forEach((element) =>
-  element.addEventListener('click', (event) => {
-    affisch.forEach((element) => {
-      element.classList.remove('remove');
-    });
-    element.classList.add('remove');
-    // let mainAffischSrc = document.getElementById('hej').getAttribute('src');
-    let src = element.getAttribute('src');
-    // element.setAttribute('src', mainAffischSrc);
-    document.querySelector('#hej').setAttribute('src', src);
-  })
-);
+// affisch.forEach((element) =>
+//   element.addEventListener('click', (event) => {
+//     affisch.forEach((element) => {
+//       element.classList.remove('remove');
+//     });
+//     element.classList.add('remove');
+//     // let mainAffischSrc = document.getElementById('hej').getAttribute('src');
+//     let src = element.getAttribute('src');
+//     // element.setAttribute('src', mainAffischSrc);
+//     document.querySelector('#hej').setAttribute('src', src);
+//   })
+// );
