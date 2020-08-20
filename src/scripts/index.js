a505a5a65c194b8ad78b59dcb96f08fd12bcd6ab
majaalin <maja_alin@hotmail.com>
2020-08-20 11:04:55 +0200

    Cleaning code and remove images

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,9 +1,6 @@
 import '../styles/index.scss';
 
-// const pil = document.querySelector('.button-wrapper');
-const flip = document.querySelector('.flip-card-inner');
 const affisch = document.querySelectorAll('.affisch');
-const links = document.querySelectorAll('.nav-link');
 
 affisch.forEach((element) =>
   element.addEventListener('click', (event) => {
@@ -11,18 +8,7 @@ affisch.forEach((element) =>
       element.classList.remove('remove');
     });
     element.classList.add('remove');
-    // let mainAffischSrc = document.getElementById('hej').getAttribute('src');
     let src = element.getAttribute('src');
-    // element.setAttribute('src', mainAffischSrc);
-    document.querySelector('#tja').setAttribute('src', src);
+    document.querySelector('#main').setAttribute('src', src);
   })
 );
-
-// links.forEach((element) =>
-//   element.addEventListener('click', (event) => {
-//     links.forEach((element) => {
-//       element.classList.remove('active');
-//     });
-//     element.classList.add('active');
-//   })
-// );
